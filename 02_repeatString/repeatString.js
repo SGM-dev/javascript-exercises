62ab8d44e88263c7c73975598b8c5037e564e705
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-11-17 23:46:55 +0530

    solve exercises for arrays and loops

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function (string, number) {
+  let result = "";
 
+  if (number < 0) {
+    return "ERROR";
+  }
+
+  let loop = number ? number : Math.floor(Math.random() * 1000);
+
+  for (let i = 0; i < number; i++) {
+    result += string;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let fahrenheit = ((temp - 32) * 5) / 9;
+  return fahrenheit ? +fahrenheit.toFixed(1) : fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let celsius = (temp * 9) / 5 + 32;
+  return celsius ? +celsius.toFixed(1) : celsius;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
